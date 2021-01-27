b8dee2b6cd39771972032228114bdf5569d99fea
zhangjinhui152 <2233274242@qq.com>
2021-01-27 16:32:12 +0800

    1.8

---
+++ js/Didplay.js
@@ -0,0 +1,7 @@
+function display() {
+    obj = document.getElementById("LinkTo2").style.display = "inline-block";
+    // alert("d");
+}
+function displayNo(){
+    document.getElementById("LinkTo2").style.display = "none";
+}
